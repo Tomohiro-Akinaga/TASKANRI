0e85e535ea09db04d649534d566497a7402758d5
Tomohiro Akinaga <tomo@TomohironoMacBook-Pro.local>
2022-07-04 19:25:22 -0700

    done sign up and log in API

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/user");
 
+//Sign up
 router.post("/signup", async (req, res) => {
     try {
         const newUser = await new User({
@@ -13,6 +14,21 @@ router.post("/signup", async (req, res) => {
         return res.status(200).json(user);
     } catch (err) {
         return req.status(500).json(err);
+    }
+});
+
+//Log in
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) return res.status(404).send("Not found User");
+
+        const validPassword = req.body.password === user.password;
+        if (!validPassword) return res.status(400).json("Wrong password");
+
+        return res.status(200).json(user);
+    } catch (err) {
+        return res.status(500).json(500);
     }
 });
 

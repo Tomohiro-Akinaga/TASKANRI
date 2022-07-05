1f48f65146379e2025417245b51e3c0ef4d29ed7
Tomohiro Akinaga <tomo@TomohironoMacBook-Pro.local>
2022-07-04 18:48:35 -0700

    connected mongoDB

--- server/server.js
+++ server/server.js
@@ -4,7 +4,20 @@ const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const saveRoute = require("./routes/save");
 const PORT = 8080;
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+//Connect DB
+mongoose
+    .connect(process.env.MONGOURL)
+    .then(() => {
+        console.log("DB is ready");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+//Route
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/save", saveRoute);

fb906b903715c2d57d834fbf9a4499cc650c0c32
Tomohiro Akinaga <tomo@TomohironoMacBook-Pro.local>
2022-07-19 18:05:34 -0700

    added local storage on SignUp component

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import UserForm from "../components/UserForm";
 
 const SignUp = () => {
+    const userid = localStorage.getItem("userid");
+    console.log(userid);
     return (
         <div>
             <UserForm />

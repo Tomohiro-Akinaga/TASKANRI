02619ee7c82aec56c823838336e2e2111f7003c4
Tomohiro Akinaga <tomo@TomohironoMacBook-Pro.local>
2022-07-28 12:00:24 -0700

    created review folder

--- client/src/helper/getLoginUserData.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export default async function getLoginUserData() {
-    const loginUserId = localStorage.getItem("userid");
-    const response = await axios.get(`/users/${loginUserId}`);
-    return response.data;
-}
